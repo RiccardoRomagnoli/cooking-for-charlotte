138b3852c245e382876f2f3575f76584b3dcfc58
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-07-24 18:48:10 +0200

    Added all the tile type for the actions and edited the creation map

--- src/it/unibo/oop18/cfc/TileMap/Tile.java
+++ src/it/unibo/oop18/cfc/TileMap/Tile.java
@@ -1,53 +1,56 @@
 package it.unibo.oop18.cfc.TileMap;
 
 import java.awt.image.BufferedImage;
+
 /**
  * Tile class.
  */
 public class Tile {
 
-        private final BufferedImage image;
-        private final int type;
+    private final BufferedImage image;
+    private final int type;
 
-        // tile types
-        // tile movement
-        public static final int NORMAL = 0;
-        public static final int BLOCKED = 1;
-        // tile food
-        public static final int MEAT = 2;
-        public static final int SALAD = 3;
-        public static final int TOMATO = 4;
-        public static final int BREAD = 5;
-        // tile action
-        public static final int CHOPPINGBOARD = 6;
-        public static final int COOKER = 7;
-        public static final int COUNTER = 8;
-        public static final int WASHBASIN = 9;
-        public static final int DISH = 10;
+    // tile types
+    // tile movement
+    public static final int NORMAL = 0;
+    public static final int BLOCKED = 1;
+    // tile food
+    public static final int SALAD = 5;
+    public static final int BREAD = 6;
+    public static final int TOMATO = 7;
+    public static final int MEAT = 8;
+    // tile action
+    public static final int DISH = 10;
+    public static final int WASHBASIN = 11;
+    public static final int COOKER = 12;
+    public static final int CHOPPINGBOARD = 13;
+    public static final int COUNTER = 14;
+    /**
+     * Constructor of the class.
+     * 
+     * @param image The image of the tile
+     * @param type  The type of the tile
+     */
+    public Tile(final BufferedImage image, final int type) {
+        this.image = image;
+        this.type = type;
+    }
 
-        /**
-           * Constructor of the class.
-           * @param image The image of the tile
-           * @param type The type of the tile
-           */
-        public Tile(final BufferedImage image, final int type) {
-            this.image = image;
-            this.type = type;
-        }
+    /**
+     * Get the image of the tile.
+     * 
+     * @return image The image of the tile
+     */
+    public BufferedImage getImage() {
+        return image;
+    }
 
-        /**
-         * Get the image of the tile.
-         * @return image The image of the tile
-         */
-        public BufferedImage getImage() {
-            return image; 
-        }
-
-        /**
-        * Get the type of the tile.
-        * @return type The type of the tile
-        */
-        public int getType() {
-            return type;
-        }
+    /**
+     * Get the type of the tile.
+     * 
+     * @return type The type of the tile
+     */
+    public int getType() {
+        return type;
+    }
 }
