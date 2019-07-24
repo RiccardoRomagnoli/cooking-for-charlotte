b8d202b13bc68dd8795630f31332c1ccdb6d4ffa
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-07-24 16:47:46 +0200

    Add all type of tile

--- src/it/unibo/oop18/cfc/TileMap/Tile.java
+++ src/it/unibo/oop18/cfc/TileMap/Tile.java
@@ -1,25 +1,53 @@
-// Only contains two types of tiles:
-// Blocked and non-blocked.
-
 package it.unibo.oop18.cfc.TileMap;
 
 import java.awt.image.BufferedImage;
-
+/**
+ * Tile class.
+ */
 public class Tile {
-	
-	private BufferedImage image;
-	private int type;
-	
-	// tile types
-	public static final int NORMAL = 0;
-	public static final int BLOCKED = 1;
-	
-	public Tile(BufferedImage image, int type) {
-		this.image = image;
-		this.type = type;
-	}
-	
-	public BufferedImage getImage() { return image; }
-	public int getType() { return type; }
-	
+
+        private final BufferedImage image;
+        private final int type;
+
+        // tile types
+        // tile movement
+        public static final int NORMAL = 0;
+        public static final int BLOCKED = 1;
+        // tile food
+        public static final int MEAT = 2;
+        public static final int SALAD = 3;
+        public static final int TOMATO = 4;
+        public static final int BREAD = 5;
+        // tile action
+        public static final int CHOPPINGBOARD = 6;
+        public static final int COOKER = 7;
+        public static final int COUNTER = 8;
+        public static final int WASHBASIN = 9;
+        public static final int DISH = 10;
+
+        /**
+           * Constructor of the class.
+           * @param image The image of the tile
+           * @param type The type of the tile
+           */
+        public Tile(final BufferedImage image, final int type) {
+            this.image = image;
+            this.type = type;
+        }
+
+        /**
+         * Get the image of the tile.
+         * @return image The image of the tile
+         */
+        public BufferedImage getImage() {
+            return image; 
+        }
+
+        /**
+        * Get the type of the tile.
+        * @return type The type of the tile
+        */
+        public int getType() {
+            return type;
+        }
 }
