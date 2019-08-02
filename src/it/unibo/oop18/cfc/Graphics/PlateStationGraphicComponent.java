bb6e30764b5fa966547c4789c6a47e5570d33790
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-02 15:09:53 +0200

    Add graphic for plate

--- src/it/unibo/oop18/cfc/Graphics/PlateStationGraphicComponent.java
+++ src/it/unibo/oop18/cfc/Graphics/PlateStationGraphicComponent.java
@@ -4,6 +4,10 @@ import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 
 import it.unibo.oop18.cfc.Objects.GameObject;
+import it.unibo.oop18.cfc.Objects.Stations.FoodStation;
+import it.unibo.oop18.cfc.Objects.Stations.PlateStation;
+import it.unibo.oop18.cfc.Tile.FoodStationTile;
+import it.unibo.oop18.cfc.Tile.PlateStationTile;
 import it.unibo.oop18.cfc.Tile.Tile;
 
 
@@ -11,19 +15,18 @@ import it.unibo.oop18.cfc.Tile.Tile;
  * This class represents still object's graphic component and models {@link GraphicsComponent}.
  */
 public class PlateStationGraphicComponent implements GraphicsComponent {
-
-    private final Tile tile;
-    private final GameObject object;
+    private final PlateStation plateStation;
+    private final PlateStationTile plateStationTile;
 
     /**
-     * Creates a {@code StillObjectGraphicComponent}.
+     * Creates a {@code DoorGraphicComponent}.
      * 
-     * @param sprite block's sprite
-     * @param object reference
+     * @param door the logic of the door
+     * @param doorSprite door's sprite
      */
-    public PlateStationGraphicComponent(final GameObject object, final Tile tile) {
-        this.tile = tile;
-        this.object = object;
+    public PlateStationGraphicComponent(final PlateStation plateStation, final PlateStationTile plateStationTile) {
+        this.plateStation = plateStation;
+        this.plateStationTile = plateStationTile;
     }
 
     /**
@@ -31,8 +34,7 @@ public class PlateStationGraphicComponent implements GraphicsComponent {
      */
     @Override
     public void draw(final Graphics2D g) {
-        g.drawImage(this.tile.getImage(),
-                AffineTransform.getTranslateInstance(this.object.getPosition().getX(),
-                                                     this.object.getPosition().getY()), null);
+        g.drawImage(this.plateStationTile.getTiles().get(0).getImage(), AffineTransform
+                .getTranslateInstance(this.plateStation.getPosition().getX(), this.plateStation.getPosition().getY()), null);
     }
 }
