df3965ee9b48c219edae55e813eec5a37f32a356
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-02 15:39:37 +0200

    Edit game main for edit GameEngine name

--- src/it/unibo/oop18/cfc/Main/Game.java
+++ src/it/unibo/oop18/cfc/Main/Game.java
@@ -7,20 +7,20 @@ package it.unibo.oop18.cfc.Main;
 import javax.swing.JFrame;
 
 public class Game {
-	
-	public static void main(String[] args) {
-		
-		JFrame window = new JFrame("Cooking for Charlotte");
-		
-		window.add(new GamePanel());
-		
-		window.setResizable(false);
-		window.pack();
-		
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
-	
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Cooking for Charlotte");
+
+        window.add(new GameEngine());
+
+        window.setResizable(false);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
 }
