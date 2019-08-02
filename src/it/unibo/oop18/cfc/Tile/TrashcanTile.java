f4b46ecf60b182331ca97a6de4e4f0971649a321
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-02 13:27:45 +0200

    Add Station Tiles

---
+++ src/it/unibo/oop18/cfc/Tile/TrashcanTile.java
@@ -0,0 +1,33 @@
+package it.unibo.oop18.cfc.Tile;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class TrashcanTile extends AbstractStationTile{
+
+     
+    private static final int Y_LOCATION = 6;
+    private static final int N_TILES = 3;
+
+    /**
+     * Creates a {@code CookerTile} container.
+     *
+     * @param sheet {@link TileSheet} where to take the {@link Tile}
+     */
+    public TrashcanTile(final TileSheet sheet) {
+        super();
+        IntStream.range(0, N_TILES).mapToObj(a -> new Tile(sheet, a, Y_LOCATION))
+                                     .collect(Collectors.toList())
+                                     .forEach(a -> super.getTiles().add(a));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getTilesNumber() {
+        return N_TILES;
+    }
+
+}
