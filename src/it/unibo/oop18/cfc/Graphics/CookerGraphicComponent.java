028be275e471919a026718e98136574db1d0b7b5
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-02 13:45:56 +0200

    Add graphic for stations

---
+++ src/it/unibo/oop18/cfc/Graphics/CookerGraphicComponent.java
@@ -0,0 +1,39 @@
+package it.unibo.oop18.cfc.Graphics;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+import it.unibo.oop18.cfc.Objects.Stations.Cooker;
+import it.unibo.oop18.cfc.Tile.CookerTile;
+
+public class CookerGraphicComponent implements GraphicsComponent{
+    private final Cooker cooker;
+    private final CookerTile cookerTile;
+
+    /**
+     * Creates a {@code DoorGraphicComponent}.
+     * 
+     * @param door the logic of the door
+     * @param doorSprite door's sprite
+     */
+    public CookerGraphicComponent(final Cooker cooker, final CookerTile cookerTile) {
+        this.cooker = cooker;
+        this.cookerTile = cookerTile;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void draw(final Graphics2D g) {
+        if (this.cooker.isCooked()) {
+            g.drawImage(this.cookerTile.getTiles().get(0).getImage(),
+                    AffineTransform.getTranslateInstance(this.cooker.getPosition().getX(), this.cooker.getPosition().getY()),
+                    null);
+        } else {
+            g.drawImage(this.cookerTile.getTiles().get(1).getImage(),
+                    AffineTransform.getTranslateInstance(this.cooker.getPosition().getX(), this.cooker.getPosition().getY()),
+                    null);
+        }
+    }
+}
---
+++ src/it/unibo/oop18/cfc/Graphics/CounterGraphicComponent.java
@@ -0,0 +1,34 @@
+package it.unibo.oop18.cfc.Graphics;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+import it.unibo.oop18.cfc.Objects.Stations.Counter;
+import it.unibo.oop18.cfc.Tile.CounterTile;
+
+public class CounterGraphicComponent implements GraphicsComponent{
+    private final Counter counter;
+    private final CounterTile counterTile;
+
+    /**
+     * Creates a {@code DoorGraphicComponent}.
+     * 
+     * @param door the logic of the door
+     * @param doorSprite door's sprite
+     */
+    public CounterGraphicComponent(final Counter counter, final CounterTile counterTile) {
+        this.counter = counter;
+        this.counterTile = counterTile;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void draw(final Graphics2D g) {
+        g.drawImage(this.counterTile.getTiles().get(0).getImage(),
+                AffineTransform.getTranslateInstance(this.counter.getPosition().getX(), this.counter.getPosition().getY()),
+                null);
+
+    }
+}
