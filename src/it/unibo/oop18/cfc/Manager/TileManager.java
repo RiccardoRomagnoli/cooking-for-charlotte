4c7ea7c227772095e41a4066612b44ca475ace88
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-02 15:23:30 +0200

    Create tile manager

--- src/it/unibo/oop18/cfc/Manager/TileManager.java
+++ src/it/unibo/oop18/cfc/Manager/TileManager.java
@@ -2,29 +2,33 @@ package it.unibo.oop18.cfc.Manager;
 
 import java.io.IOException;
 
-import game.graphics.Sprite;
-import game.graphics.SpriteSheet;
+import it.unibo.oop18.cfc.Tile.ChoppingStationTile;
+import it.unibo.oop18.cfc.Tile.CookerTile;
+import it.unibo.oop18.cfc.Tile.CounterTile;
+import it.unibo.oop18.cfc.Tile.DeliveryStationTile;
+import it.unibo.oop18.cfc.Tile.FoodStationTile;
+import it.unibo.oop18.cfc.Tile.PlateStationTile;
+import it.unibo.oop18.cfc.Tile.TileSheet;
+import it.unibo.oop18.cfc.Tile.TrashcanTile;
+import it.unibo.oop18.cfc.Tile.WashbasinTile;
+
+
+
+
 
 /**
  * This class stores and gets any kind of sprite in game.
  */
 public class TileManager {
 
-    private static final int Y_LOCATION = 1;
-    private static final int WALL_X_LOCATION = 1;
-    private static final int BREAKABLE_WALL_X_LOCATION = 2;
-    private static final int KEY_X_LOCATION = 3;
-    private static final int FLOOR_X_LOCATION = 6;
-    private static final int ENEMY_Y_LOCATION = 7;
-    private static final int GHOST_Y_LOCATION = 13;
-    private static final int SMART_ENEMY_LOCATION = 18;
-
-    private final Sprite wall;
-    private final Sprite breakableWall;
-    private final Sprite key;
-    private final Sprite floor;
-
-
+    private final ChoppingStationTile choppingStationTile;
+    private final CookerTile cookerTile;
+    private final CounterTile counterTile;
+    private final DeliveryStationTile deliveryStationTile;
+    private final FoodStationTile foodStationTile;
+    private final PlateStationTile plateStationTile;
+    private final TrashcanTile trashcanTile;
+    private final WashbasinTile washbasinTile;
     /**
      * Creates a {@code SpritesManager} to manage any {@link Sprite} in game.
      *
@@ -32,117 +36,63 @@ public class TileManager {
      * @throws IOException : problem during input/output
      */
     public TileManager(final String folder) throws IOException {
-        final TileSheet sheet = new TileSheet(folder + "/spriteSheet.png");
-        this.breakableWall = new Sprite(sheet, BREAKABLE_WALL_X_LOCATION, Y_LOCATION);
-        this.wall = new Sprite(sheet, WALL_X_LOCATION, Y_LOCATION);
-        this.key = new Sprite(sheet, KEY_X_LOCATION, Y_LOCATION);
-        this.floor = new Sprite(sheet, FLOOR_X_LOCATION, Y_LOCATION);
-        this.door = new DoorSprite(sheet);
-        this.player = new PlayerSprites(sheet);
-        this.enemy = new EnemySprites(sheet, ENEMY_Y_LOCATION);
-        this.bomb = new ChoppingStation(sheet);
-        this.flame = new FlameSprites(sheet);
-        this.ghost = new EnemySprites(sheet, GHOST_Y_LOCATION);
-        this.smartEnemy = new EnemySprites(sheet, SMART_ENEMY_LOCATION);
+        final TileSheet sheet = new TileSheet(folder + "/tileSheet.png");
+        this.choppingStationTile = new ChoppingStationTile(sheet);
+        this.cookerTile = new CookerTile(sheet);
+        this.counterTile = new CounterTile(sheet);
+        this.deliveryStationTile = new DeliveryStationTile(sheet);
+        this.foodStationTile = new FoodStationTile(sheet);
+        this.plateStationTile = new PlateStationTile(sheet);
+        this.trashcanTile = new TrashcanTile(sheet);
+        this.washbasinTile = new WashbasinTile(sheet);
     }
-
     /**
-     * Gets the wall sprite.
-     *
-     * @return wall {@link Sprite}
+     * @return the choppingStationTile
      */
-    public Sprite getWallSprite() {
-        return this.wall;
+    public ChoppingStationTile getChoppingStationTile() {
+        return choppingStationTile;
     }
-
     /**
-     * Gets the breakable wall sprite.
-     *
-     * @return breakable wall {@link Sprite}.
+     * @return the cookerTile
      */
-    public Sprite getBreakableWallSprite() {
-        return this.breakableWall;
+    public CookerTile getCookerTile() {
+        return cookerTile;
     }
-
     /**
-     * Gets the key sprite.
-     *
-     * @return key {@link Sprite}
+     * @return the counterTile
      */
-    public Sprite getKeySprite() {
-        return this.key;
+    public CounterTile getCounterTile() {
+        return counterTile;
     }
-
     /**
-     * Gets the floor sprite.
-     *
-     * @return floor {@link Sprite}
+     * @return the deliveryStationTile
      */
-    public Sprite getFloorSprite() {
-        return this.floor;
+    public DeliveryStationTile getDeliveryStationTile() {
+        return deliveryStationTile;
     }
-
     /**
-     * Gets the door sprite container.
-     *
-     * @return {@link DoorSprite}
+     * @return the foodStationTile
      */
-    public DoorSprite getDoorSprite() {
-        return this.door;
+    public FoodStationTile getFoodStationTile() {
+        return foodStationTile;
     }
-
     /**
-     * Gets the player sprite container.
-     *
-     * @return {@link PlayerSprites}
+     * @return the plateStationTile
      */
-    public PlayerSprites getPlayerSprite() {
-        return this.player;
+    public PlateStationTile getPlateStationTile() {
+        return plateStationTile;
     }
-
     /**
-     * Gets the enemy sprite container.
-     *
-     * @return {@link EnemySprites}
+     * @return the trashcanTile
      */
-    public EnemySprites getEnemySprite() {
-        return this.enemy;
+    public TrashcanTile getTrashcanTile() {
+        return trashcanTile;
     }
-
     /**
-     * Gets the ghost sprite container.
-     *
-     * @return ghost {@link EnemySprites}
+     * @return the washbasinTile
      */
-    public EnemySprites getGhost() {
-        return this.ghost;
-    }
-
-    /**
-     * Gets the smart enemy sprite container.
-     *
-     * @return smart enemy {@link EnemySprites}
-     */
-    public EnemySprites getSmartEnemy() {
-        return this.smartEnemy;
-    }
-
-    /**
-     * Gets bomb sprite container.
-     *
-     * @return {@link ChoppingStation}
-     */
-    public ChoppingStation getBomb() {
-        return this.bomb;
-    }
-
-    /**
-     * Gets flame sprite container.
-     *
-     * @return {@link FlameSprites}
-     */
-    public FlameSprites getFlame() {
-        return this.flame;
+    public WashbasinTile getWashbasinTile() {
+        return washbasinTile;
     }
 
 }
