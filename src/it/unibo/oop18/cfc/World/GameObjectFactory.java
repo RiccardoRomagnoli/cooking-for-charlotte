6a5606323f6ba18ff1c65280b255c7f223092e82
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-02 19:24:14 +0200

    Create GameObjectFactory

---
+++ src/it/unibo/oop18/cfc/World/GameObjectFactory.java
@@ -0,0 +1,100 @@
+package it.unibo.oop18.cfc.World;
+
+import it.unibo.oop18.cfc.Objects.Entity.PlayerImpl;
+import it.unibo.oop18.cfc.Objects.Stations.ChoppingStation;
+import it.unibo.oop18.cfc.Objects.Stations.Cooker;
+import it.unibo.oop18.cfc.Objects.Stations.Counter;
+import it.unibo.oop18.cfc.Objects.Stations.DeliveryStation;
+import it.unibo.oop18.cfc.Objects.Stations.FoodStation;
+import it.unibo.oop18.cfc.Objects.Stations.PlateStation;
+import it.unibo.oop18.cfc.Objects.Stations.Trashcan;
+import it.unibo.oop18.cfc.Objects.Stations.Washbasin;
+import it.unibo.oop18.cfc.Util.Position;
+
+/**
+ * This interface declares the method of a GameObjectFactory.
+ */
+public interface GameObjectFactory {
+
+    /**
+     * Creates a {@link PlayerImpl}.
+     * 
+     * @param position player's position.
+     * @param world    the world where to add the player
+     * @return a new player
+     */
+    //PlayerImpl createPlayer(Position position, World world);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    ChoppingStation createChoppingBoard(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    Cooker createCooker(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    Counter createCounter(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    DeliveryStation createDeliveryStation(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    PlateStation createPlateStation(Position position);    
+    
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    FoodStation createFoodStation(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    Trashcan createTrashcan(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    Washbasin createWashbasin(Position position);
+
+    /**
+     * Creates an unbreakable {@link Block}.
+     * 
+     * @param position block's position
+     * @return a new unbreakable Block
+     */
+    // Floor createFloor(Position position);
+
+}
---
+++ src/it/unibo/oop18/cfc/World/GameObjectFactoryImpl.java
@@ -0,0 +1,79 @@
+package it.unibo.oop18.cfc.World;
+
+import it.unibo.oop18.cfc.Manager.TileManager;
+import it.unibo.oop18.cfc.Objects.Stations.ChoppingStation;
+import it.unibo.oop18.cfc.Objects.Stations.Cooker;
+import it.unibo.oop18.cfc.Objects.Stations.Counter;
+import it.unibo.oop18.cfc.Objects.Stations.DeliveryStation;
+import it.unibo.oop18.cfc.Objects.Stations.FoodStation;
+import it.unibo.oop18.cfc.Objects.Stations.PlateStation;
+import it.unibo.oop18.cfc.Objects.Stations.Trashcan;
+import it.unibo.oop18.cfc.Objects.Stations.Washbasin;
+import it.unibo.oop18.cfc.Util.Position;
+
+/**
+ * This class models {@link GameObjectFactory}.
+ */
+public class GameObjectFactoryImpl implements GameObjectFactory {
+
+    private String folder;
+    //private SpriteManager sm;
+    private TileManager tm;
+    /**
+     * Creates a {@code GameObjectFactoryImpl}.
+     * 
+     * @param theme the theme of objects' sprite
+     */
+    public GameObjectFactoryImpl(final String folder) {
+        this.folder = folder;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+//    @Override
+//    public PlayerImpl createPlayer(final Position position, final World world) {
+//        return new PlayerImpl(new Position(position), this.theme.getSprites().getPlayerSprite(), world);
+//    }
+
+    @Override
+    public ChoppingStation createChoppingBoard(Position position) {
+        return new ChoppingStation(new Position(position), this.tm.getChoppingStationTile());
+    }
+
+    @Override
+    public Cooker createCooker(Position position) {
+        return new Cooker(new Position(position), this.tm.getCookerTile());
+    }
+
+    @Override
+    public Counter createCounter(Position position) {
+        return new Counter(new Position(position), this.tm.getCounterTile());
+    }
+
+    @Override
+    public DeliveryStation createDeliveryStation(Position position) {
+        return new DeliveryStation(new Position(position), this.tm.getDeliveryStationTile());
+    }
+
+    @Override
+    public FoodStation createFoodStation(Position position) {
+        return new FoodStation(new Position(position), this.tm.getFoodStationTile());
+    }
+
+    @Override
+    public Trashcan createTrashcan(Position position) {
+        return new Trashcan(new Position(position), this.tm.getTrashcanTile());
+    }
+
+    @Override
+    public Washbasin createWashbasin(Position position) {
+        return new Washbasin(new Position(position), this.tm.getWashbasinTile());
+    }
+
+    @Override
+    public PlateStation createPlateStation(Position position) {
+        return new PlateStation(new Position(position), this.tm.getPlateStationTile());
+    }
+
+}
