d46c87dbe901ea7389df7a901b8aa6d34fad3fe3
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-03 02:43:35 +0200

    Add hud to draw

--- src/it/unibo/oop18/cfc/GameState/PlayState.java
+++ src/it/unibo/oop18/cfc/GameState/PlayState.java
@@ -8,6 +8,8 @@ package it.unibo.oop18.cfc.GameState;
 import java.awt.Graphics2D;
 import java.io.IOException;
 
+import it.unibo.oop18.cfc.HUD.DownHud;
+import it.unibo.oop18.cfc.HUD.TopHud;
 import it.unibo.oop18.cfc.Manager.GameStateManager;
 import it.unibo.oop18.cfc.World.WorldImpl;
 
@@ -15,7 +17,8 @@ import it.unibo.oop18.cfc.World.WorldImpl;
 public class PlayState extends GameState {
 
     private WorldImpl world;
-
+    private TopHud topHud;
+    private DownHud downHud;
     public PlayState(GameStateManager gsm) {
         super(gsm, GameStates.PLAY);
     }
@@ -25,6 +28,8 @@ public class PlayState extends GameState {
     public void init(){
         try {
             this.world = new WorldImpl();
+            this.topHud = new TopHud(this);
+            this.downHud = new DownHud(this, 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,5 +41,7 @@ public class PlayState extends GameState {
 
     public void draw(Graphics2D g) {
         world.draw(g);
+        topHud.draw(g);
+        downHud.draw(g);
     }
 }
