9f2cf3a5fa0caccbffafc57059148dda3b058b6b
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-07 02:33:37 +0200

    Edited but probably never used in future

--- src/it/unibo/oop18/cfc/Graphics/IngredientGraphicComponent.java
+++ src/it/unibo/oop18/cfc/Graphics/IngredientGraphicComponent.java
@@ -2,12 +2,21 @@ package it.unibo.oop18.cfc.Graphics;
 
 import java.awt.Graphics2D;
 
+import it.unibo.oop18.cfc.Objects.Items.IngredientImpl;
+import it.unibo.oop18.cfc.Objects.Items.IngredientState;
+import it.unibo.oop18.cfc.Sprite.IngredientSprite;
+
 public class IngredientGraphicComponent implements GraphicsComponent{
 
+    IngredientImpl ingredient;
+    IngredientSprite ingredientSprite;
+    
+    
     @Override
     public void draw(Graphics2D g) {
-        // TODO Auto-generated method stub
-        
+        if(ingredient.getState() == IngredientState.RAW) {
+
+        }
     }
 
 }
