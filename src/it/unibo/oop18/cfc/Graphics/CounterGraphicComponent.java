71d1f57ff80611f0fd634d972894d92f12e4bd87
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-09 19:46:44 +0200

    Edit position of items

--- src/it/unibo/oop18/cfc/Graphics/CounterGraphicComponent.java
+++ src/it/unibo/oop18/cfc/Graphics/CounterGraphicComponent.java
@@ -5,6 +5,7 @@ import java.awt.geom.AffineTransform;
 
 import it.unibo.oop18.cfc.Tile.TileSheet;
 import it.unibo.oop18.cfc.Util.Position;
+import it.unibo.oop18.cfc.Main.GameEngine;
 import it.unibo.oop18.cfc.Objects.Stations.Counter;
 import it.unibo.oop18.cfc.Tile.CounterTile;
 
@@ -12,8 +13,15 @@ public class CounterGraphicComponent implements GraphicsComponent {
     private final Counter counter;
     private final CounterTile counterTile;
 
-    /**
-     * Creates a CounterGraphicComponent
+    private static final int POSITION_X_ITEM = 8;
+    private static final int POSITION_Y_ITEM = -2;
+    private static final int HEIGHT_ITEM = 30;
+    private static final int WIDTH_ITEM = 30;
+    private static final int POSITION_X_ITEM1 = 5;
+    private static final int POSITION_Y_ITEM1 = 13;
+
+    /** 
+     * Creates a CounterGraphicComponent.
      * 
      * @param counter Object Station
      * @param counterTile Tile of the Station
@@ -28,8 +36,8 @@ public class CounterGraphicComponent implements GraphicsComponent {
      */
     @Override
     public void draw(final Graphics2D g) {
-        if (this.counter.getPosition().getX() == 0 || this.counter.getPosition().getX() == 960) {
-            if (this.counter.getPosition().getY() == 576) {
+        if (this.counter.getPosition().getX() == 0 || this.counter.getPosition().getX() == GameEngine.WIDTH - TileSheet.TILE_SIZE_IN_GAME) {
+            if (this.counter.getPosition().getY() == GameEngine.HEIGHT2 - TileSheet.TILE_SIZE_IN_GAME) {
                 g.drawImage(this.counterTile.getTiles().get(2).getImage(), AffineTransform.getTranslateInstance(
                         this.counter.getPosition().getX(), this.counter.getPosition().getY()), null);
 
@@ -37,7 +45,10 @@ public class CounterGraphicComponent implements GraphicsComponent {
                 g.drawImage(this.counterTile.getTiles().get(1).getImage(), AffineTransform.getTranslateInstance(
                         this.counter.getPosition().getX(), this.counter.getPosition().getY()), null);
                 if (this.counter.getItem().isPresent()) {
-                    this.counter.getItem().get().draw(g, this.counter.getPosition());
+                    this.counter.getItem().get().draw(g,
+                            new Position(counter.getPosition().getX() + POSITION_X_ITEM1,
+                                    counter.getPosition().getY() + POSITION_Y_ITEM1),
+                            WIDTH_ITEM, HEIGHT_ITEM);
                 }
             }
         } else {
@@ -45,9 +56,9 @@ public class CounterGraphicComponent implements GraphicsComponent {
                     .getTranslateInstance(this.counter.getPosition().getX(), this.counter.getPosition().getY()), null);
             if (this.counter.getItem().isPresent()) {
                 this.counter.getItem().get().draw(g,
-                        new Position(counter.getPosition().getX() + TileSheet.TILE_SIZE_IN_GAME / 2 - 30/2,
-                                counter.getPosition().getY() + 28/2 - 30/2),
-                        30, 30);
+                        new Position(counter.getPosition().getX() + POSITION_X_ITEM,
+                                counter.getPosition().getY() + POSITION_Y_ITEM),
+                        WIDTH_ITEM, HEIGHT_ITEM);
             }
         }
     }
