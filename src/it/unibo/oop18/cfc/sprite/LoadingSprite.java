bd9a72bbc82d5f9299eafc24be237f5ac2dbc427
Andrea Pulita <andrea.pulita@studio.unibo.it>
2019-08-11 20:02:24 +0200

    Edit

--- src/it/unibo/oop18/cfc/sprite/LoadingSprite.java
+++ src/it/unibo/oop18/cfc/sprite/LoadingSprite.java
@@ -7,25 +7,25 @@ import java.util.stream.IntStream;
 
 public class LoadingSprite extends AbstractItemSprite{
 
-    private static final int LOADING_SPRITES = 4;
+    private static final int LOAD_SPRITES = 4;
     private static final int Y_LOCATION_LOADING = 6;
 
-    private final List<ItemSprite> loadingSprite;
+    private final List<ItemSprite> loadingSprites;
 
     public LoadingSprite(final ItemSpriteSheet sheet) {
         super();
-        this.loadingSprite = new ArrayList<>();
-        IntStream.range(0, LOADING_SPRITES).mapToObj(a -> new ItemSprite(sheet, a, Y_LOCATION_LOADING)).collect(Collectors.toList())
-        .forEach(a -> loadingSprite.add(a));
+        this.loadingSprites = new ArrayList<>();
+        IntStream.range(0, LOAD_SPRITES).mapToObj(a -> new ItemSprite(sheet, a, Y_LOCATION_LOADING)).collect(Collectors.toList())
+        .forEach(a -> loadingSprites.add(a));
     }
 
     @Override
     public int getItemSpriteNumber() {
-        return LOADING_SPRITES;
+        return LOAD_SPRITES;
     }
 
     public List<ItemSprite> getLoadingSprite() {
-        return loadingSprite;
+        return loadingSprites;
     }
 
 }
