900a40749679f4f7f244deda5ccedc5b29174d72
Davide Schiaroli <davide.schiaroli2@studio.unibo.it>
2019-08-13 22:37:32 +0200

    Started to create unit test of scoring

---
+++ src/it/unibo/oop18/cfc/test/TestScore.java
@@ -0,0 +1,42 @@
+package it.unibo.oop18.cfc.test;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+import it.unibo.oop18.cfc.util.GameScoreImpl;
+
+/**
+ * TODO .
+ *
+ *
+ */
+public class TestScore {
+
+    private static final int SCORE_1 = 50;
+
+    /**
+     * Test the correct generation of an order.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void scoreTest() throws IOException {
+        final GameScoreImpl score = new GameScoreImpl();
+        score.computeScore(0);
+        Assert.assertTrue(score.getScore() == 0);
+        score.computeScore(SCORE_1);
+        score.computeScore(SCORE_1);
+        score.computeScore(-SCORE_1 * 2);
+        Assert.assertTrue(score.getScore() == 0);
+    }
+
+    /**
+     * @exception Exception gamescore setted but not created
+     */
+    @Test(expected = Exception.class)
+    public void testScore() throws Exception {
+        final GameScoreImpl score = new GameScoreImpl();
+        Assert.assertTrue(score.getScore() == 0);
+        score.getScore();
+    }
+}
