3cf79576ea8e43680f63e95ce5b3d1877eb6dd22
Riccardo Romagnoli <riccardo.romagnoli3@studio.unibo.it>
2019-08-17 17:35:25 +0200

    Removed unused method from interface

--- src/it/unibo/oop18/cfc/object/items/OrderIngredient.java
+++ src/it/unibo/oop18/cfc/object/items/OrderIngredient.java
@@ -11,41 +11,12 @@ import it.unibo.oop18.cfc.util.Position;
 public interface OrderIngredient {
 
     /**
-     * Gets the ingredient.
-     *
-     * @return the type of the ingredient
-     */
-    IngredientType getIngredient();
-
-    /**
-     * Gets the state.
-     *
-     * @return the current state of the Ingredient
-     */
-    IngredientState getState();
-
-    /**
      * Check ingredient.
      *
      * @param ingredient the {@link Ingredient}
      * @return the {@link CheckStatus}
      */
     CheckStatus checkIngredient(Ingredient ingredient);
-
-    /**
-     * Check if ingredient state match his state.
-     * 
-     * @return true, if match
-     */
-    boolean isReady();
-
-    /**
-     * Draw the ingredient.
-     *
-     * @param g the {@link Graphics2D} of the screen
-     * @param p the {@link Position}
-     */
-    void draw(Graphics2D g, Position p);
 
     /**
      * Draw the ingredient with different dimension.
