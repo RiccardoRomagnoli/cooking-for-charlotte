fd15f5d6bb5dc51bd46449f848862b9db538369b
luca <luca.rossi57@studio.unibo.it>
2019-08-20 12:27:39 +0200

    added tests on TestIngredient

--- src/it/unibo/oop18/cfc/test/TestIngredient.java
+++ src/it/unibo/oop18/cfc/test/TestIngredient.java
@@ -2,6 +2,7 @@ package it.unibo.oop18.cfc.test;
 
 import it.unibo.oop18.cfc.manager.GameStateManager;
 import it.unibo.oop18.cfc.manager.ItemManager;
+import it.unibo.oop18.cfc.object.items.Ingredient;
 import it.unibo.oop18.cfc.object.items.IngredientImpl;
 import it.unibo.oop18.cfc.object.items.IngredientState;
 import it.unibo.oop18.cfc.object.items.IngredientType;
@@ -26,17 +27,23 @@ public class TestIngredient {
           final World world = new WorldImpl(new GameStateManager()); 
           final ItemManager im = world.getItemManager();
           // creation of the ingredients
-          final IngredientImpl bread = new IngredientImpl(im, IngredientType.BREAD);
-          final IngredientImpl lettuce = new IngredientImpl(im, IngredientType.LETTUCE);
-          final IngredientImpl tomato = new IngredientImpl(im, IngredientType.TOMATO);
-          final IngredientImpl meat = new IngredientImpl(im, IngredientType.MEAT);
-          final IngredientImpl cookedMeat = new IngredientImpl(im, IngredientType.MEAT, IngredientState.PERFECT);
+          final Ingredient bread = new IngredientImpl(im, IngredientType.BREAD);
+          final Ingredient lettuce = new IngredientImpl(im, IngredientType.LETTUCE);
+          final Ingredient tomato = new IngredientImpl(im, IngredientType.TOMATO);
+          final Ingredient meat = new IngredientImpl(im, IngredientType.MEAT);
+          final Ingredient cookedMeat = new IngredientImpl(im, IngredientType.MEAT, IngredientState.PERFECT);
           //testing the right creation
           Assert.assertTrue(bread.getState() == IngredientState.RAW);
           Assert.assertTrue(lettuce.getState() == IngredientState.RAW);
           Assert.assertTrue(tomato.getState() == IngredientState.RAW);
           Assert.assertTrue(meat.getState() == IngredientState.RAW);
           Assert.assertTrue(cookedMeat.getState() == IngredientState.PERFECT);
+          //testing that the points are right
+          Assert.assertTrue(bread.getState().getPoints() == IngredientState.RAW.getPoints());
+          Assert.assertTrue(lettuce.getState().getPoints() == IngredientState.RAW.getPoints());
+          Assert.assertTrue(tomato.getState().getPoints() == IngredientState.RAW.getPoints());
+          Assert.assertTrue(meat.getState().getPoints() == IngredientState.RAW.getPoints());
+          Assert.assertTrue(cookedMeat.getState().getPoints() == IngredientState.PERFECT.getPoints());
           //testing that the states change right 
           bread.changeState(IngredientState.CHOPPED);
           lettuce.changeState(IngredientState.CHOPPED);
@@ -46,5 +53,10 @@ public class TestIngredient {
           Assert.assertTrue(lettuce.getState() == IngredientState.CHOPPED);
           Assert.assertTrue(tomato.getState() == IngredientState.CHOPPED);
           Assert.assertTrue(meat.getState() == IngredientState.PERFECT);
+          //testing that the changes are right
+          Assert.assertTrue(bread.getState().getPoints() == IngredientState.CHOPPED.getPoints());
+          Assert.assertTrue(lettuce.getState().getPoints() == IngredientState.CHOPPED.getPoints());
+          Assert.assertTrue(tomato.getState().getPoints() == IngredientState.CHOPPED.getPoints());
+          Assert.assertTrue(meat.getState().getPoints() == IngredientState.PERFECT.getPoints());
     }
 }
